83aa0acee8ea7a923bddbcd37693d31f4acdaacb
Karandeep <jinnie1996@hotmail.com>
2022-02-07 11:09:59 -0800

    making api routes

--- python-project/app/api/post_routes.py
+++ python-project/app/api/post_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, session, request
-from app.models import User, db
+from app.models import User, Post, Follow, db
 from flask_login import current_user, login_required
 
 post_routes = Blueprint('posts', __name__)
@@ -8,18 +8,19 @@ post_routes = Blueprint('posts', __name__)
 @post_routes.route('/')
 @login_required
 def photoFeed():
-    return
-
+    posts = Post.query.all()
+    if User.
 
 @post_routes.route('/<int:id>')
-def getOnePost():
-    return
+def getOnePost(id):
+    post = Post.query.get(id)
+    return post
 
 
 @post_routes.route('/create', methods=["POST"])
 @login_required
 def newPost():
-    return
+
 
 
 @post_routes.route('/<int:id>', methods=["PUT"])
@@ -30,5 +31,8 @@ def editPost():
 
 @post_routes.route('/<int:id>', methods=["DELETE"])
 @login_required
-def deletePost():
+def deletePost(id):
+    post = Post.query.get(id)
+    db.session.delete(post)
+    db.session.commit()
     return
