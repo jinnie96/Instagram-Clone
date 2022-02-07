1be40722c02791d24d295bdb452953be4247ca26
Karandeep <jinnie1996@hotmail.com>
2022-02-07 00:45:41 -0800

    fixed api routes

--- python-project/app/api/comment_routes.py
+++ python-project/app/api/comment_routes.py
@@ -2,20 +2,20 @@ from flask import Blueprint, jsonify, session, request
 from app.models import User, db
 from flask_login import current_user, login_user, logout_user, login_required
 
-comment_routes = Blueprint('comments', __name__)
+comment_routes = Blueprint('comments', __name__, url_prefix='/comments')
 
-@comment_routes.route('/p/<int:id>/comments')
+@comment_routes.route('/<int:id>/comments')
 def getComment():
     return
 
-@comment_routes.route('/p/<int:id>/comments', methods=["POST"])
+@comment_routes.route('/<int:id>/comments', methods=["POST"])
 def newComment():
     return
 
-@comment_routes.route('/comments/<int:id>', methods=["PUT"])
+@comment_routes.route('/<int:id>', methods=["PUT"])
 def editComment():
     return
 
-@comment_routes.route('/comments/<int:id>', methods=["DELETE"])
+@comment_routes.route('/<int:id>', methods=["DELETE"])
 def deleteComment():
     return
--- python-project/app/api/post_routes.py
+++ python-project/app/api/post_routes.py
@@ -2,9 +2,9 @@ from flask import Blueprint, jsonify, session, request
 from app.models import User, db
 from flask_login import current_user, login_user, logout_user, login_required
 
-post_routes = Blueprint('posts', __name__)
+post_routes = Blueprint('posts', __name__, url_prefix='/posts')
 
-@post_routes.route('/<int:id>/posts')
+@post_routes.route('/')
 def photoFeed():
     return
 
@@ -12,10 +12,10 @@ def photoFeed():
 def newPost():
     return
 
-@post_routes.route('/p/<int:id>', methods=["PUT"])
+@post_routes.route('/<int:id>', methods=["PUT"])
 def editPost():
     return
 
-@post_routes.route('/p/<int:id>', methods=["DELETE"])
+@post_routes.route('/<int:id>', methods=["DELETE"])
 def deletePost():
     return
