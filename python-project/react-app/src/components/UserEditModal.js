85ab7795d10793e3a231d0103fc03ccf9a4c3973
Jake <jweber04005@msn.com>
2022-02-10 13:25:33 -0500

    Merge Main

---
+++ python-project/react-app/src/components/UserEditModal.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from 'react-redux'
+import { Modal } from '../context/Modal'
+
+
+const EditProfileModal = ({ setShowModal, user }) => {
+    const [username, setUsername] = useState(user.username)
+    const [firstName, setFirstName] = useState(user.first_name)
+    const [lastName, setLastName] = useState(user.last_name)
+    const [email, setEmail] = useState(user.email)
+    const [biography, setBiography] = useState(user.biography)
+
+    const dispatch = useDispatch()
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const formData = new FormData()
+    }
+
+    const handleCancel = (e) => {
+        e.preventDefault()
+        return setShowModal(false)
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <input
+                    name='username'
+                    type='text'
+                    value={username || ""}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <input
+                    name='first_name'
+                    type='text'
+                    value={firstName || ""}
+                    onChange={(e) => setFirstName(e.target.value)}
+                />
+                <input
+                    name='last_name'
+                    type='text'
+                    value={lastName || ""}
+                    onChange={(e) => setLastName(e.target.value)}
+                />
+                <input
+                    name='email'
+                    type='text'
+                    value={email || ""}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    name='biography'
+                    type='textarea'
+                    value={biography || ""}
+                    onChange={(e) => setBiography(e.target.value)}
+                />
+                <button type="submit">Submit</button>
+                <button onClick={handleCancel}>Cancel</button>
+            </form>
+        </>
+    )
+}
+
+export default EditProfileModal
