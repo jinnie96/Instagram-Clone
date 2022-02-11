1cc45b7e3f815e33b80d52dbbf619939a365e999
Karandeep <jinnie1996@hotmail.com>
2022-02-10 16:54:25 -0800

    fixed the profile pictures. shows images

--- python-project/react-app/src/components/ProfilePage/UserEditModal.js
+++ python-project/react-app/src/components/ProfilePage/UserEditModal.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from 'react-redux'
-import { Modal } from '../context/Modal'
-import * as userActions from '../store/users'
+import { Modal } from '../../context/Modal'
+import * as userActions from '../../store/users'
 
 
 const EditProfileModal = ({ setShowModal, user, setUpdate }) => {
