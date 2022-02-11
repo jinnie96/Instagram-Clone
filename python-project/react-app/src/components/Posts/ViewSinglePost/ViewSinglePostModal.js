13f6ec2b6c23579c60da3b86d99bf9460040951f
Savanah Trewman <strewman@me.com>
2022-02-11 11:39:17 -0600

    styled individual posts on home page

--- python-project/react-app/src/components/Posts/ViewSinglePost/ViewSinglePostModal.js
+++ python-project/react-app/src/components/Posts/ViewSinglePost/ViewSinglePostModal.js
@@ -9,8 +9,8 @@ import './ViewSinglePost.css';
 const ViewSinglePost = ({ post, comments }) => {
     console.log("Post and Comment", post, comments);
     return (
-        <div className='single-post-container' key={post.id}>
-            <div className='post-image'
+        <div className='home-single-container' key={post.id}>
+            <div className='home-single-image'
                 style={{
                     backgroundImage: `url(${post.image})`,
                     backgroundSize: "cover",
@@ -18,18 +18,23 @@ const ViewSinglePost = ({ post, comments }) => {
                     backgroundRepeat: "no-repeat",
                     backgroundPosition: "center"
                 }}></div>
-            <div className='post-caption'>
-                <div className='post-username'>{post.username}</div>
-                <div className='caption-username'>{post.username}</div>
-                <div className='caption-caption'>{post.caption}</div>
-            </div>
-            <div>
-                {comments.comments.map(comment => (
-                    <CommentDetails comment={comment} key={comment.id}/>
-                ))}
-                <button>Like</button>
-                <CreateComment post={post}/>
-            </div>
+            <span className='home-single-span'>
+                <div className='home-single-info'>
+                    <div id='home-single-username'>{post.username}</div>
+                    <div id='home-single-caption'>
+                        <p><b>{post.username}</b> {post.caption}</p>
+                    </div>
+                </div>
+                <div className='home-single-comments'>
+                    {comments.comments.map(comment => (
+                        <CommentDetails comment={comment} key={comment.id}/>
+                    ))}
+                </div>
+                <div className='home-single-create'>
+                    <button>Like</button>
+                    <CreateComment post={post} />
+                </div>
+            </span>
         </div>
     )
 }
