81c6ec6f4852ca5d9e26e5f502058f137f595f1a
Jake <jweber04005@msn.com>
2022-02-14 12:02:39 -0500

    Fixed Error Handling

--- python-project/app/forms/signup_form.py
+++ python-project/app/forms/signup_form.py
@@ -24,7 +24,7 @@ class SignUpForm(FlaskForm):
     username = StringField('username', validators=[DataRequired(), username_exists])
     first_name = StringField('first_name', validators=[DataRequired()])
     last_name = StringField('last_name', validators=[DataRequired()])
-    email = StringField('email', validators=[DataRequired(), user_exists])
+    email = StringField('email', validators=[DataRequired(), user_exists, Email()])
     biography = StringField('biography')
     profile_picture = TextAreaField('profile_picture')
     password = StringField('password', validators=[DataRequired()])
