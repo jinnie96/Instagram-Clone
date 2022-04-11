d14fdf7d1ddb0e0feb1038a19a130d2ce28e4789
Jake <jweber04005@msn.com>
2022-04-11 12:31:23 -0400

    AWS Account Reactivated

--- python-project/app/seeds/posts.py
+++ python-project/app/seeds/posts.py
@@ -2,19 +2,19 @@ from app.models import db, Post
 
 def seed_posts():
     post1 = Post(
-        user_id = 1, image = 'https://python-group-project-insta-clone.s3.amazonaws.com/69ef0da56ca24091a2a712ab7b5281c9.jpeg', caption = 'Test Post'
+        user_id=1, image='https://python-group-project-insta-clone1.s3.amazonaws.com/dog-3071334_960_720.jpg', caption='Dog'
     )
 
     post2 = Post(
-        user_id = 2, image = 'https://python-group-project-insta-clone.s3.amazonaws.com/2703be206de64f77bc836b39cc619f30.png', caption = "Test Post 12"
+        user_id=2, image='https://python-group-project-insta-clone1.s3.amazonaws.com/frog-2240764_960_720.jpg', caption="Frog"
     )
 
     post3 = Post(
-        user_id = 3, image = 'https://python-group-project-insta-clone.s3.amazonaws.com/12966ea60b5344b9bcdf20241e34d8ae.jpg', caption = "Test Post 12"
+        user_id=3, image='https://python-group-project-insta-clone1.s3.amazonaws.com/key-3348307_960_720.jpg', caption="Key"
     )
 
     post4 = Post(
-        user_id = 3, image = 'https://python-group-project-insta-clone.s3.amazonaws.com/98ebf54901d34033b7ee76e1fb33422d.jpeg', caption = "Test Post 12"
+        user_id=3, image='https://python-group-project-insta-clone1.s3.amazonaws.com/pyrenees-351266_960_720.jpg', caption="Space"
     )
 
 
