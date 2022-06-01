ac2be89cc787c9e26816bb46639554c50eccd91f
Karandeep <jinnie1996@hotmail.com>
2022-06-01 00:42:39 -0700

    aws

--- python-project/app/seeds/posts.py
+++ python-project/app/seeds/posts.py
@@ -2,19 +2,19 @@ from app.models import db, Post
 
 def seed_posts():
     post1 = Post(
-        user_id = 1, image = 'https://python-group-project-insta-clone.s3.amazonaws.com/69ef0da56ca24091a2a712ab7b5281c9.jpeg', caption = 'Test Post'
+        user_id = 1, image = 'https://instaclone-bucket-app.s3.us-west-2.amazonaws.com/FullSizeSynth.pdf', caption = 'piano'
     )
 
     post2 = Post(
-        user_id = 2, image = 'https://python-group-project-insta-clone.s3.amazonaws.com/2703be206de64f77bc836b39cc619f30.png', caption = "Test Post 12"
+        user_id = 2, image = 'https://instaclone-bucket-app.s3.us-west-2.amazonaws.com/Photo2.jpeg', caption = "lights"
     )
 
     post3 = Post(
-        user_id = 3, image = 'https://python-group-project-insta-clone.s3.amazonaws.com/12966ea60b5344b9bcdf20241e34d8ae.jpg', caption = "Test Post 12"
+        user_id = 3, image = 'https://instaclone-bucket-app.s3.us-west-2.amazonaws.com/Photo5.jpeg', caption = "party"
     )
 
     post4 = Post(
-        user_id = 3, image = 'https://python-group-project-insta-clone.s3.amazonaws.com/98ebf54901d34033b7ee76e1fb33422d.jpeg', caption = "Test Post 12"
+        user_id = 3, image = 'https://instaclone-bucket-app.s3.us-west-2.amazonaws.com/Photo8.jpeg', caption = "hey"
     )
 
 
